202e3143c6b60e674be33466beab4d5b9e7b0b8d
thiha <brthiha@gmail.com>
2012-10-27 01:29:30 +0800

    Revert "first time"
    This reverts commit 1afada12036d478183e5f84154c7dad469a20e16.

--- application/config/email.php
+++
@@ -1,8 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-
-$config['protocol'] = 'smtp';
-$config['smtp_host'] = 'ssl://smtp.googlemail.com';
-$config['smtp_port'] = 465;
-$config['smtp_user'] = 'brthiha@gmail.com';
-$config['smtp_pass'] = 'Thihasoe125464mon';
--- application/views/fragment/category_input_fragment.php
+++
@@ -1,3 +0,0 @@
-<input type="hidden" name="categoryid" value="{categoryid}" id="cagtegoryid">
-<label for="">Category name</label>
-<p><input type="text" name="name" value="{name}" id="name" size="40" maxlength="39"></p>
